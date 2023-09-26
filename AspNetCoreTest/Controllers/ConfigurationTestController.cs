2161f1cc7aff856ccd06469430418495c0e50687
Reno <patrenowork@outlook.com>
2023-09-26 17:13:01 +0200

    Get secrets

--- AspNetCoreTest/Controllers/ConfigurationTestController.cs
+++ AspNetCoreTest/Controllers/ConfigurationTestController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 
 namespace AspNetCoreTest.Controllers
@@ -21,6 +24,31 @@ namespace AspNetCoreTest.Controllers
             var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")??"";
 
             return $"{environment} - {message}";
+        }
+
+        [HttpGet("GetTestSecret")]
+        public string GetTestSecret()
+        {
+            SecretClientOptions options = new SecretClientOptions()
+            {
+                Retry =
+                {
+                    Delay= TimeSpan.FromSeconds(2),
+                    MaxDelay = TimeSpan.FromSeconds(16),
+                    MaxRetries = 5,
+                    Mode = RetryMode.Exponential
+                 }
+            };
+
+            string vaultName = _configuration["KeyVaultName"];
+
+            var client = new SecretClient(new Uri($"https://{vaultName}.vault.azure.net/"), new DefaultAzureCredential(), options);
+
+            KeyVaultSecret secret = client.GetSecret("mySecret");
+
+            string secretValue = secret.Value;
+
+            return secretValue;
         }
     }
 }
